fd748468a05e26aece0c36d613c0607897c15a43
Alex Dallaway [work] <alex.dallaway@gmail.com>
2013-01-04 10:12:17 -0500

    Some formatting tweaks

--- views/elements/queries.html.php
+++ views/elements/queries.html.php
@@ -1,12 +1,30 @@
 <?php
 $total_queries = count($queries);
 $total_query_time = 0;
-foreach($queries as $query) {
-	$total_query_time += !empty($query['explain']['millis']) ?
-		$query['explain']['millis'] :
-		0;
+foreach($queries as $query)
+{
+  $total_query_time += !empty($query['explain']['millis']) ? $query['explain']['millis'] : 0;
 }
 ?>
 <h2>Queries</h2>
-<p>There <?php echo ($total_queries == 1) ? 'is':'are'; ?> <span class="li3-perf-stat-value"><?=$total_queries; ?></span> quer<?php echo ($total_queries == 1) ? 'y':'ies'; ?> which took <span class="li3-perf-stat-value"><?=$total_query_time; ?>ms</span> to execute.</p>
-<?=$this->li3perf->printVars($queries); ?>
+<p>There <?php echo ($total_queries == 1) ? 'is':'are'; ?> <span class="li3-perf-stat-value"><?=$total_queries; ?></span> quer<?php echo ($total_queries == 1) ? 'y':'ies'; ?> which took <span class="li3-perf-stat-value"><?= round($total_query_time, 2); ?>ms</span> to execute.</p>
+
+<table>
+<?php
+foreach($queries as $query)
+{
+?>
+  <tr>
+    <td>Ms: <?= round($query['explain']['millis'], 2); ?>ms</td>
+    <td>Params: <?= implode(', ', $query['params']); ?></td>
+    <td>Types: <?= implode(', ', $query['types']); ?></td>
+  </tr>
+  <tr>
+    <td colspan="3">
+      <?= $query['sql']; ?>
+    </td>
+  </tr>
+<?php
+}
+?>
+</table>
