a51cf7c531ab7ef6217f1ea585203f1611ee0d16
Alex Dallaway [work] <alex.dallaway@gmail.com>
2013-01-04 16:30:14 -0500

    Changed debug dump

--- views/elements/variables.html.php
+++ views/elements/variables.html.php
@@ -9,8 +9,11 @@ $variable_size = $this->li3perf->byteSize($this->li3perf->varSize($vars['view'])
 <h2>View Template Variables</h2>
 <p>There <?php echo ($total_vars == 1) ? 'is':'are'; ?> <span class="li3-perf-stat-value"><?=$total_vars; ?></span> variable<?php echo ($total_vars == 1) ? '':'s'; ?> occupying about <span class="li3-perf-stat-value"><?=$variable_size; ?></span> in memory.</p>
 <?php
-foreach($vars['view'] as $k => $v) {
-	echo '<span class="li3-perf-stat-value">$' . $k . '</span><br />';
-	echo $this->li3perf->printVars($v);
+foreach($vars['view'] as $k => $v)
+{
+  echo '<span class="li3-perf-stat-value">$' . $k . '</span><br />';
+  echo '<pre>';
+  Doctrine\Common\Util\Debug::dump($v, 1);
+  echo '</pre>';
 }
 ?>
